744e9ab30634dfce93a45ef25aa00f3090a5da13
Mateus Pereira <mateus_pereira7@hotmail.com>
2018-04-20 14:29:16 -0300

    Startign database scripts

---
+++ count.py
@@ -0,0 +1,12 @@
+with open('../pan11-intrinsic-plagiarism-detection-test-corpus-2011-05-13/suspicious-documents/suspicious-document03103.txt') as infile:
+    lines=0
+    words=0
+    characters=0
+    for line in infile:
+        wordslist=line.split()
+        lines=lines+1
+        words=words+len(wordslist)
+        characters += sum(len(word) for word in wordslist)
+print(lines)
+print(words)
+print(characters)
