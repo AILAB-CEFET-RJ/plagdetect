3a4261eeb91628dfbca6869153296e6d2d2f197a
Mateus Pereira <madpprofile@gmail.com>
2018-05-01 13:59:13 -0300

    Dataset generation changes (efficiency and Python 2)

--- generate-dataset.py
+++ generate-dataset.py
@@ -8,15 +8,17 @@ def create_folder(directory):
 		if e.errno != errno.EEXIST:
 			raise
 
-def generate_file(c2, c3, doc_id):
-	c2.execute('SELECT id, fragment, isplag FROM sentence where fk_article_id = ? ORDER BY offset', (doc_id,))
-	#if c2.rowcount > 0:
-	with open(os.path.join(directory, 'suspicious-document' + f'{doc_id:05}' + '.txt'), 'w', encoding='utf-8') as f:
-		for row in c2:
-			c3.execute('SELECT fragment, isplag FROM sentence where fk_article_id = ? AND id > ? ORDER BY offset', (doc_id, row[0]))
-			for row_ahead in c3:
-				same_style = '1' if row[2] == row_ahead[1] else '0'
-				f.write(row[1].replace('\n', '').replace('\r', '') + '\t' + row_ahead[0].replace('\n', '').replace('\r', '') + '\t' + same_style + '\n')
+def generate_file(doc_id):
+	c.execute('SELECT id, fragment, isplag FROM sentence where fk_article_id = ? ORDER BY offset', (doc_id,))
+	lines = c.fetchall()
+	with open(os.path.join(directory, 'suspicious-document' + '{:05d}'.format(doc_id) + '.txt'), 'w', encoding='utf-8') as f:
+		print('Generating %s', f.name)
+		for line in lines:
+			for line_ahead in lines[lines.index(line)+1:]:
+				if line[2] == line_ahead[2] and line[2] == True:
+					continue
+				same_style = '1' if line[2] == line_ahead[2] else '0'
+				f.write(line[1] + '\t' + line_ahead[1] + '\t' + same_style + '\n')
 
 
 if __name__ == '__main__':
@@ -29,11 +31,13 @@ if __name__ == '__main__':
 	try:
 		db = lite.connect(db_filename)
 		c = db.cursor()
-		c2 = db.cursor()
-		c3 = db.cursor()
-		c.execute('SELECT DISTINCT fk_article_id FROM sentence')
-		for row in c:
-			generate_file(c2, c3, row[0])
+		sql = 'SELECT DISTINCT fk_article_id FROM sentence'
+		if len(sys.argv) == 2:
+			sql += ' WHERE fk_article_id <= ' + sys.argv[1]
+		c.execute(sql)
+		article_ids = c.fetchall()
+		for article_id in article_ids:
+			generate_file(article_id[0])
 
 	except lite.Error as e:
 		print("Error %s:" % e.args[0])
@@ -41,9 +45,5 @@ if __name__ == '__main__':
 	finally:
 		if c:
 			c.close()
-		if c2:
-			c2.close()
-		if c3:
-			c3.close()
 		if db:
 			db.close()
