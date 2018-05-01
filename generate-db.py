a74bdb87be72bc12a3227f9f1fa2a18fbea76015
Mateus Pereira <madpprofile@gmail.com>
2018-05-01 18:47:00 -0300

    Adding list of authors to be excluded

--- generate-db.py
+++ generate-db.py
@@ -1,6 +1,6 @@
 import sqlite3 as lite
 import xml.etree.ElementTree as et
-import sys, os, errno
+import sys, os, errno, codecs
 import nltk.data
 
 
@@ -72,14 +72,14 @@ def populate_tables(c, tokenizer):
 		file = os.path.join('dataset', file) # Get path that works for Windows and Linux
 		if file.endswith('.txt'):
 			xmlfile = file.replace('.txt', '.xml')
-			with open(file) as f:
-				data = f.read().decode("utf-8-sig")
+			with codecs.open(file, encoding='utf-8-sig') as f:
+				data = f.read()
 				print(f.name)
 				tree = et.parse(xmlfile)
 				root = tree.getroot()
 				plags = []
 				for feature in root:
-					if 'authors' in feature.attrib:
+					if 'authors' in feature.attrib and feature.attrib['authors'] not in ignore_list:
 						author = feature.attrib['authors']
 						article_id = insert_into_article_table(c, f, author)
 					elif 'name' in feature.attrib and feature.attrib['name'] == 'plagiarism':
@@ -96,11 +96,29 @@ def create_views(c):
 	sql = '''create view articles_per_author as select author, count(author) as number_of_articles from article group by author;'''
 	c.execute(sql)
 
+def get_ignore_list():
+	return [
+		'American Tract Society',
+		'Consumers\' League of New York, The',
+		'Guaranty Trust Company of New York',
+		'Teachers of the School Street Universalist Sunday School, Boston',
+		'Three Initiates',
+		'United States Patent Office',
+		'United States.Army.Corps of Engineers.Manhattan District',
+		'United States.Congress.House.Committee on Science and Astronautics.',
+		'United States.Dept.of Defense',
+		'United States.Executive Office of the President',
+		'United States.Presidents.',
+		'Work Projects Administration',
+
+	]
 
 if __name__ == '__main__':
 	db_filename = 'plag.db'
 	remove(db_filename)
 	db = None
+	global ignore_list
+	ignore_list = get_ignore_list()
 	try:
 		db = lite.connect(db_filename)
 		c = db.cursor()
