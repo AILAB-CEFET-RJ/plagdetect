a43e7e93ffbd48ac87f5c683422010dfa561e6ff
Mateus Pereira <madpprofile@gmail.com>
2018-05-10 22:39:21 -0300

    Changing dataset generation to a single compressed file on bz2 format.

--- generate-dataset.py
+++ generate-dataset.py
@@ -1,5 +1,5 @@
 import sqlite3 as lite
-import os, sys, errno, codecs
+import os, sys, errno, bz2file
 
 def create_folder(directory):
 	try:
@@ -8,17 +8,17 @@ def create_folder(directory):
 		if e.errno != errno.EEXIST:
 			raise
 
-def generate_file(doc_id):
+def generate_file(f, doc_id):
 	c.execute('SELECT id, fragment, isplag FROM sentence where fk_article_id = ? ORDER BY offset', (doc_id,))
 	lines = c.fetchall()
-	with codecs.open(os.path.join(directory, 'suspicious-document' + '{:05d}'.format(doc_id) + '.txt'), 'w', encoding='utf-8') as f:
-		print('Generating %s', f.name)
-		for line in lines:
-			for line_ahead in lines[lines.index(line)+1:]:
-				if line[2] == line_ahead[2] and line[2] == True:
-					continue
-				same_style = '1' if line[2] == line_ahead[2] else '0'
-				f.write('\t'.join([line[1], line_ahead[1], same_style]) + '\n')
+	print('Generating for doc_id ', doc_id)
+	for line in lines:
+		for line_ahead in lines[lines.index(line)+1:]:
+			if line[2] == line_ahead[2] and line[2] == True:
+				continue
+			same_style = '1' if line[2] == line_ahead[2] else '0'
+			f.write(('\t'.join([line[1], line_ahead[1], same_style]) + '\n').encode('utf-8'))
+	f.flush()
 
 
 if __name__ == '__main__':
@@ -32,12 +32,15 @@ if __name__ == '__main__':
 		db = lite.connect(db_filename)
 		c = db.cursor()
 		sql = 'SELECT DISTINCT fk_article_id FROM sentence'
-		if len(sys.argv) == 2:
-			sql += ' WHERE fk_article_id <= ' + sys.argv[1]
+		if len(sys.argv) >= 2:
+			sql += ' WHERE fk_article_id >= ' + sys.argv[1]
+		if len(sys.argv) >= 3:
+			sql += ' and fk_article_id <= ' + sys.argv[2]
 		c.execute(sql)
 		article_ids = c.fetchall()
-		for article_id in article_ids:
-			generate_file(article_id[0])
+		with bz2file.open(os.path.join(directory, 'generated-dataset.bz2'), 'w', 9) as f:
+			for article_id in article_ids:
+				generate_file(f, article_id[0])
 
 	except lite.Error as e:
 		print("Error %s:" % e.args[0])
