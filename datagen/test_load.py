6e1f81686ee8e8523275d6819d7d7abcc96f27f1
Mateus Pereira <mateus_pereira7@hotmail.com>
2018-06-20 12:41:33 -0300

    Adding count to test_load script

--- datagen/test_load.py
+++ datagen/test_load.py
@@ -1,13 +1,21 @@
 import sqlite3 as lite
-from generate_dataset import get_id_dataset, get_sentences_hashmap
+from generate_dataset import get_id_dataset, get_sentences_hashmap, get_id_dataset_size
 
 #edited
 db = lite.connect('../plag.db')
 c = db.cursor()
+
+print 'Loading hashmap'
 hashmap = get_sentences_hashmap(c)
-print 'Hashmap loaded in memory'
-get_id_dataset(c)
+print 'Hashmap loaded'
+
+print 'Executing count SQL'
+size = get_id_dataset(c)
+print 'Number of tuples'
+print size
+
 print 'SQL executed'
+get_id_dataset(c)
 tuples = c.fetchall()
 print 'Tuples loaded in memory'
 raw_input("Test completed. Press Enter to continue...")
