c0ac3a4fdf469b849d4698f6de574a3929abcb72
Mateus Pereira <mateus_pereira7@hotmail.com>
2018-06-20 12:26:15 -0300

    Adding prints to test_load script.

--- datagen/test_load.py
+++ datagen/test_load.py
@@ -5,6 +5,10 @@ from generate_dataset import get_id_dataset, get_sentences_hashmap
 db = lite.connect('../plag.db')
 c = db.cursor()
 hashmap = get_sentences_hashmap(c)
+print 'Hashmap loaded in memory'
 get_id_dataset(c)
-tuples = c.execute().fetchall()
+print 'SQL executed'
+tuples = c.fetchall()
+print 'Tuples loaded in memory'
+raw_input("Test completed. Press Enter to continue...")
 
